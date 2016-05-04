05dbb5d071d885b2b214391506edc93699f9ebcb
ZhenShiErGe <895990111@qq.com>
2016-05-04 21:00:51 +0800

    uodate

--- SimpleConnnectionPool/src/com/xyz/test/Demo.java
+++ SimpleConnnectionPool/src/com/xyz/test/Demo.java
@@ -4,22 +4,9 @@ package com.xyz.test;
 import com.xyz.connpool.IConnPoolManager;
 import com.xyz.connpool.IConnection;
 import com.xyz.connpool.impl.ConnPoolManager;
-import com.xyz.transaction.DataSourceHandler;
-import com.xyz.transaction.TransactionTemplate;
 
 public class Demo{
-	//类比框架注入的数据源
-//	public static IConnPoolManager dataSource=new ConnPoolManager(3, 5, 1000);
 	public static void main(String [] args) throws Exception{
-//		IConnPoolManager dataSourceProxy=(IConnPoolManager) new DataSourceHandler().bind(dataSource);
-//		final IConnection conn=dataSourceProxy.getConn(0);
-//		(new TransactionTemplate() {
-//			@Override
-//			public void doInTransaction() {
-//				// TODO Auto-generated method stub
-//				new Service().service(conn);
-//			}
-//		}).doTransaction(conn);
 		final IConnPoolManager pools=new ConnPoolManager(3, 5, 5000);
 		for(int i=0;i<9;i++){
 			new Thread(new Runnable() {
